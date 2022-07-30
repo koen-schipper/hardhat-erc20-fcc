9cdb801a5b08e463687fe93e9b5948b6a32a0867
Koen Schipper <koen.schipper@me.com>
2022-07-30 12:03:09 +0200

    Update OurToken.sol

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20{
-    constructor (uint256 initialSupply) ERC20("OurToken", "OT") {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
         _mint(msg.sender, initialSupply);
     }
 }
